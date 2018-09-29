16a0982e64fe9e50c45227133863bd2709e28bc6
Maksim Kostromin <daggerok@gmail.com>
2018-09-30 02:23:14 +0300

    Add some event-sourcing...

--- src/main/java/daggerok/App.kt
+++ src/main/java/daggerok/App.kt
@@ -1,5 +1,6 @@
 package daggerok
 
+import org.hibernate.annotations.GenericGenerator
 import org.springframework.context.annotation.Bean
 import org.springframework.http.MediaType.*
 import org.springframework.web.reactive.function.server.ServerResponse.ok
@@ -9,9 +10,12 @@ import reactor.core.publisher.toMono
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.core.io.ClassPathResource
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 import org.springframework.web.reactive.function.server.RenderingResponse.create
 import org.springframework.web.reactive.function.server.ServerResponse
-import reactor.core.publisher.Mono
+import reactor.core.publisher.toFlux
+import reactor.core.scheduler.Schedulers.elastic
 
 import javax.persistence.*
 import java.sql.Timestamp
@@ -19,49 +23,44 @@ import java.time.Instant
 import java.util.UUID
 
 @Entity
-class EventStore {
-  @Id
-  var id: UUID
-    protected set
-  var aggregateId: UUID
-    protected set
-  var aggregateType: String
-    protected set
-  var payLoad: String
-    protected set
-  var version: Int = 0
+data class EventStore(
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    var id: String? = null,
+    var aggregateId: UUID? = null,
+    @Version var version: Int = 0,
+    val events: ArrayList<String> = arrayListOf("created"),
+    var createdAt: Timestamp? = null,
+    var updatedAt: Timestamp? = null
+) {
 
-  @Basic
-  protected var createdAt: Timestamp? = null
-
-  constructor() {
-    this.id = UUID.randomUUID()
-    this.version = 1
-  }
-
-  constructor(id: UUID, aggregateId: UUID, aggregateType: String, payLoad: String, version: Int) {
-    this.id = id
-    this.aggregateId = aggregateId
-    this.aggregateType = aggregateType
-    this.payLoad = payLoad
-    this.version = version
-  }
-
-  fun getCreatedAt(): String {
-    return if (createdAt != null) {
-      createdAt!!.toString()
-    } else createdAt!!.toString()
-
+  fun update(): EventStore {
+    events.add("updated")
+    return this
   }
 
   @PrePersist
   fun prePersist() {
     this.createdAt = Timestamp.from(Instant.now())
+    this.createdAt = this.createdAt?.clone() as Timestamp?
+  }
+
+  @PreUpdate
+  fun preUpdate() {
+    this.updatedAt = Timestamp.from(Instant.now())
   }
 }
 
+@Repository interface EventStoreRepository : JpaRepository<EventStore, String> {
+  fun findFirstByIdOrAggregateId(id: String, aggregateId: UUID): List<EventStore>
+}
+
+fun String.toUUID(): UUID = try { UUID.fromString(this) }
+catch (e: IllegalArgumentException) { UUID.fromString("00000000-0000-0000-0000-000000000000") }
+
 @SpringBootApplication
-class App {
+class App(val eventStoreRepository: EventStoreRepository) {
 
   @Bean
   fun routes() = router {
@@ -71,12 +70,30 @@ class App {
         //ok().render("index", mapOf("message" to "ololo trololo"))
         create("index")
             .modelAttribute("message", "ololo trololo")
-            .build() as Mono<ServerResponse>
+            .build()
+            .cast(ServerResponse::class.java)
       }
       contentType(APPLICATION_JSON_UTF8)
+      POST("/api/{uuid}") {
+        val id = it.pathVariable("uuid")
+        val uuid = id.toUUID()
+        ok().body(
+            eventStoreRepository
+                .findFirstByIdOrAggregateId(id, uuid)
+                .toFlux()
+                .map { eventStoreRepository.save(it.update()) }
+                .subscribeOn(elastic())
+        )
+      }
+      POST("/api/**") {
+        ok().body(mapOf("aggregate" to eventStoreRepository
+            .save(EventStore(aggregateId = UUID.randomUUID()))).toMono())
+      }
       GET("/api/**") {
         ok().body(
-            mapOf("hello" to "world").toMono()
+            eventStoreRepository
+                .findAll()
+                .toFlux()
         )
       }
     }
