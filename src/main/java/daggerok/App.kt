852df91d901247d90a3e1251e083110ef7c591e3
Maksim Kostromin <daggerok@gmail.com>
2018-09-30 01:09:29 +0300

    Update versions.

--- src/main/java/daggerok/App.kt
+++ src/main/java/daggerok/App.kt
@@ -13,6 +13,53 @@ import org.springframework.web.reactive.function.server.RenderingResponse.create
 import org.springframework.web.reactive.function.server.ServerResponse
 import reactor.core.publisher.Mono
 
+import javax.persistence.*
+import java.sql.Timestamp
+import java.time.Instant
+import java.util.UUID
+
+@Entity
+class EventStore {
+  @Id
+  var id: UUID
+    protected set
+  var aggregateId: UUID
+    protected set
+  var aggregateType: String
+    protected set
+  var payLoad: String
+    protected set
+  var version: Int = 0
+
+  @Basic
+  protected var createdAt: Timestamp? = null
+
+  constructor() {
+    this.id = UUID.randomUUID()
+    this.version = 1
+  }
+
+  constructor(id: UUID, aggregateId: UUID, aggregateType: String, payLoad: String, version: Int) {
+    this.id = id
+    this.aggregateId = aggregateId
+    this.aggregateType = aggregateType
+    this.payLoad = payLoad
+    this.version = version
+  }
+
+  fun getCreatedAt(): String {
+    return if (createdAt != null) {
+      createdAt!!.toString()
+    } else createdAt!!.toString()
+
+  }
+
+  @PrePersist
+  fun prePersist() {
+    this.createdAt = Timestamp.from(Instant.now())
+  }
+}
+
 @SpringBootApplication
 class App {
 
